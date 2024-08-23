ec4a01bbe49deebb03b1c4f838ec341e27036c1e
xialio <a1356281880@hotmail.com>
2024-08-23 22:52:39 +0800

    Site updated: 2024-08-23 22:52:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/hello-world/"];function toRandomPost(){
+var posts=["2020/01/01/hello-world/","2024/08/23/page/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
