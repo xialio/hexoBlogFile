cbb44da660b733c1f8e79c43b3165b0a789f15ae
xialio <a1356281880@hotmail.com>
2024-08-24 01:38:20 +0800

    Site updated: 2024-08-24 01:38:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2020/01/01/hello-world/","2024/08/23/nahida/","2024/08/23/z1girl/","2020/01/01/00def/"];function toRandomPost(){
+var posts=["2020/01/01/00def/","2020/01/01/hello-world/","2024/08/23/nahida/","2024/08/23/z1girl/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
